8224fd8d1a4bcb91639a50e7a1892e97c01357dc
Tchouela eunice <tchouelaeunice.questia@ictuniversity.edu.cm>
2026-06-12 10:50:32 +0100

    Add temporary backend server

--- backend/server.js
+++ backend/server.js
@@ -1,1 +1,10 @@
-console.log('AgroNexus backend running')
+const http = require('http');
+
+const server = http.createServer((req, res) => {
+  res.writeHead(200, {'Content-Type': 'text/plain'});
+  res.end('AgroNexus backend running');
+});
+
+server.listen(3000, () => {
+  console.log('AgroNexus backend running on http://localhost:3000');
+});
