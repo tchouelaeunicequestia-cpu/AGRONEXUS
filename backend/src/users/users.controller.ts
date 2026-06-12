8c8ff560cbd92941e37a7333cd2d7d36679f7b00
Tchouela eunice <tchouelaeunice.questia@ictuniversity.edu.cm>
2026-06-12 19:05:13 +0100

    Add create and list users endpoints

--- backend/src/users/users.controller.ts
+++ backend/src/users/users.controller.ts
@@ -1,4 +1,18 @@
-import { Controller } from '@nestjs/common';
+import { Body, Controller, Get, Post } from '@nestjs/common';
+import { UsersService } from './users.service';
+import { User } from './user.entity';
 
 @Controller('users')
-export class UsersController {}
+export class UsersController {
+  constructor(private readonly usersService: UsersService) {}
+
+  @Post()
+  createUser(@Body() data: Partial<User>) {
+    return this.usersService.create(data);
+  }
+
+  @Get()
+  getUsers() {
+    return this.usersService.findAll();
+  }
+}
